ba925130cb5b92991019369072e0924f8b73b071
Aditya <adityaadak2002@gmail.com>
2024-07-21 21:15:48 +0530

    Updated code with new features

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
     <>
       <header className="header">
         <nav className="nav container">
-          <a href="index.html" className="nav__logo">Aditya</a>
+          <a href="index.html" className="nav__logo">BYTEL_link</a>
             
           <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
             <ul className="nav__list grid">
